c72dd3aa247f43b468e0528a8385493e2bb07a28
Galangkangin Gotera <galangkangingotera@hotmail.com>
2020-06-30 21:46:27 +0800

    OS201 thegalang

---
+++ answer.sh
@@ -0,0 +1,26 @@
+export CEKSAYA="thegalang ZCZCSCRIPTSTART 200608-151821\nthegalang thegalang 200608-151851-b68b-UAS/ 200608-151851 SEQOK SUMOK b68bcd18\nthegalang thegalang 200608-151859-24bd-UAS/ 200608-151859 SEQOK SUMOK 24bdc4e9\nthegalang thegalang 200608-151902-a327-UAS/ 200608-151902 SEQOK SUMOK a327320c\nthegalang thegalang 200608-151911-d931-UAS/ 200608-151902 SEQOK SUMOK d93183a2\nthegalang ZCZCSCRIPTSTOP 200608 151915 SEQOK"
+
+export CEKSEBELA1="thegalang ZCZCSCRIPTSTART 200531-133456\nthegalang titusdirgantoro 200531-133530-3d4d-titus.dirgantoro/ 200531-133530 SEQOK SUMOK 3d4d757f\nthegalang titusdirgantoro 200531-133535-b9dd-titus.dirgantoro/ 200531-133535 SEQOK SUMOK b9dd823e\nthegalang titusdirgantoro 200531-133542-b99b-titus.dirgantoro/ 200531-133542 SEQOK SUMOK b99b674d\nthegalang titusdirgantoro 200531-133549-1596-titus.dirgantoro/ 200531-133549 SEQOK SUMOK 1596753f\nthegalang ZCZCSCRIPTSTOP 200531-133556 SEQOK"
+
+export CEKSEBELA2="thegalang ZCZCSCRIPTSTART 200605-020903\nthegalang tolhassianipar 200605-020945-27ed-tolhas.parulian/ 200605-020945 SEQOK SUMOK 27edff59\nthegalang tolhassianipar 200605-021027-9e78-tolhas.parulian/ 200605-021027 SEQOK SUMOK 9e78d28f\nthegalang tolhassianipar 200605-021033-3864-tolhas.parulian/ 200605-021033 SEQOK SUMOK 3864700b\nthegalang tolhassianipar 200605-021051-90f8-tolhas.parulian/ 200605-021051 SEQOK SUMOK 90f863bd\nthegalang ZCZCSCRIPTSTOP 200605-021230 SEQOK"
+
+export CEKSAYA="thegalang ZCZCSCRIPTSTART 200608-151821\n
+thegalang thegalang 200608-151851-b68b-UAS/ 200608-151851 SEQOK SUMOK b68bcd18\n
+thegalang thegalang 200608-151859-24bd-UAS/ 200608-151859 SEQOK SUMOK 24bdc4e9\n
+thegalang thegalang 200608-151902-a327-UAS/ 200608-151902 SEQOK SUMOK a327320c\n
+thegalang thegalang 200608-151911-d931-UAS/ 200608-151902 SEQOK SUMOK d93183a2\n
+thegalang ZCZCSCRIPTSTOP 200608 151915 SEQOK"
+
+export CEKSEBELA1="thegalang ZCZCSCRIPTSTART 200531-133456\n
+thegalang titusdirgantoro 200531-133530-3d4d-titus.dirgantoro/ 200531-133530 SEQOK SUMOK 3d4d757f\n
+thegalang titusdirgantoro 200531-133535-b9dd-titus.dirgantoro/ 200531-133535 SEQOK SUMOK b9dd823e\n
+thegalang titusdirgantoro 200531-133542-b99b-titus.dirgantoro/ 200531-133542 SEQOK SUMOK b99b674d\n
+thegalang titusdirgantoro 200531-133549-1596-titus.dirgantoro/ 200531-133549 SEQOK SUMOK 1596753f\n
+thegalang ZCZCSCRIPTSTOP 200531-133556 SEQOK"
+
+export CEKSEBELA2="thegalang ZCZCSCRIPTSTART 200605-020903\n
+thegalang tolhassianipar 200605-020945-27ed-tolhas.parulian/ 200605-020945 SEQOK SUMOK 27edff59\n
+thegalang tolhassianipar 200605-021027-9e78-tolhas.parulian/ 200605-021027 SEQOK SUMOK 9e78d28f\n
+thegalang tolhassianipar 200605-021033-3864-tolhas.parulian/ 200605-021033 SEQOK SUMOK 3864700b\n
+thegalang tolhassianipar 200605-021051-90f8-tolhas.parulian/ 200605-021051 SEQOK SUMOK 90f863bd\n
+thegalang ZCZCSCRIPTSTOP 200605-021230 SEQOK"
